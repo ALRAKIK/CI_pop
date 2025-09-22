980b806c46c49330c9ad2cd48a67afc11364fe4c
Amer ALRAKIK <alrakikamer@gmail.com>
2025-09-22 13:52:05 +0200

    increase the number of terms in the sum

--- src/Toroidal/1D/intregrals/ERI_integrals_toroidal.f90
+++ src/Toroidal/1D/intregrals/ERI_integrals_toroidal.f90
@@ -100,7 +100,7 @@ subroutine ERI_integral_4_function_toroidal(one,two,three,four,value)
 
               test = dexp(-(alpha+beta-mu_x)*(Lx**2)/(2.d0*pi**2)) * dexp(-(gamma+delta-nu_x)*(Lx**2)/(2.d0*pi**2))
 
-              if (test < 1e-10) cycle
+              if (test < 1e-15) cycle
 
               xpA     = ax*(xp - xa)
               xpB     = ax*(xp - xb) 
@@ -149,7 +149,7 @@ subroutine integrate_ERI_mod_mod(pattern_id,p,q,p_x,q_x,phi,xpA,xpB,xqC,xqD,xa,x
     
       ! Local variables
 
-      double precision,parameter         :: epsabs = 1.0e-8 , epsrel = 1.0e-6
+      double precision,parameter         :: epsabs = 1.0e-10 , epsrel = 1.0e-6
       integer,parameter                  :: inf = 1 
       double precision,parameter         :: bound = 0.0d0
       integer, parameter                 :: limit = 50
@@ -190,7 +190,7 @@ subroutine integrate_ERI_mod_mod(pattern_id,p,q,p_x,q_x,phi,xpA,xpB,xqC,xqD,xa,x
       double precision                     :: D, D2, ax2 
       double precision                     :: t2, t4 
       double precision                     :: const 
-      double precision                     :: tol  = 1D-10
+      double precision                     :: tol  = 1D-15
       COMPLEX(KIND=KIND(1.0D0)), PARAMETER :: I_dp = (0.0D0, 1.0D0)
       integer                              :: n, small_count
       COMPLEX(KIND=KIND(1.0D0))            :: termAn, termBn
