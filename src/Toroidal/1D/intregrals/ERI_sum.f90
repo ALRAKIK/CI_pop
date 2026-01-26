d58bad068c69d790371c8ffda4e4be0a0140ff0a
Amer ALRAKIK <alrakikamer@gmail.com>
2026-01-26 13:28:06 +0100

    try to solve numerical unstability on the cluster

--- src/Toroidal/1D/intregrals/ERI_sum.f90
+++ src/Toroidal/1D/intregrals/ERI_sum.f90
@@ -1472,6 +1472,7 @@ subroutine integrate_ERI_sum(pattern_id,p,q,p_x,q_x,phi,xpA,xpB,xqC,xqD,xa,xb,xc
       sum         = inv_ax2 * (cxpa * cxpb * iv_scaled(n,A)-c2xpab*(0.25d0*(iv_scaled(n-2,A)+2.d0*iv_scaled(n,A)+iv_scaled(n+2,A)))) * inv_ax2 * (cxqc*cxqd*iv_scaled(n,B)-c2xqcd*(0.25d0*(iv_scaled(n-2,B)+2.d0*iv_scaled(n,B)+iv_scaled(n+2,B)))) * iv_scaled(n, C) * const
       Peak        = ceiling(min(A,B,C))
       Nmax        = Peak+10
+      print*, "sum ", sum
       do n = 1 , Nmax
         if (p_x < 1.d0-10) then  
         termAn  = inv_ax2 * (cxpa * cxpb * iv_scaled(n,A)-c2xpab*(0.25d0*(iv_scaled(n-2,A)+2.d0*iv_scaled(n,A)+iv_scaled(n+2,A))))
