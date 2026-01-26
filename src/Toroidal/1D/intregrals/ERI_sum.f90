a2e4d86a4b366531634e38a39415429352dd009e
Amer ALRAKIK <alrakikamer@gmail.com>
2026-01-26 20:11:20 +0100

    try to solve numerical unstability on the cluster

--- src/Toroidal/1D/intregrals/ERI_sum.f90
+++ src/Toroidal/1D/intregrals/ERI_sum.f90
@@ -1477,12 +1477,12 @@ subroutine integrate_ERI_sum(pattern_id,p,q,p_x,q_x,phi,xpA,xpB,xqC,xqD,xa,xb,xc
       Peak        = ceiling(min(A,B,C))
       Nmax        = Peak+10
       do n = 1 , Nmax
-        if (p_x < 1.d0-10) then  
+        if (dabs(A) < 1.d0-10) then  
         termAn  = inv_ax2 * (cxpa * cxpb * iv_scaled(n,A)-c2xpab*(0.25d0*(iv_scaled(n-2,A)+2.d0*iv_scaled(n,A)+iv_scaled(n+2,A))))
         else 
         termAn  = inv_ax2 * (cxpa * cxpb * iv_scaled(n,A)-c2xpab*(0.25d0*(iv_scaled(n-2,A)+2.d0*iv_scaled(n,A)+iv_scaled(n+2,A)))+I_dp/A*n * s2xpab * (0.5d0*(iv_scaled(n-1,A)+iv_scaled(n+1,A))))
         end if 
-        if (q_x < 1.d0-10) then 
+        if (dabs(B) < 1.d0-10) then 
         termBn  = inv_ax2 * (cxqc * cxqd * iv_scaled(n,B)-c2xqcd*(0.25d0*(iv_scaled(n-2,B)+2.d0*iv_scaled(n,B)+iv_scaled(n+2,B))))
         else 
         termBn  = inv_ax2 * (cxqc * cxqd * iv_scaled(n,B)-c2xqcd*(0.25d0*(iv_scaled(n-2,B)+2.d0*iv_scaled(n,B)+iv_scaled(n+2,B)))-I_dp/B*n * s2xqcd * (0.5d0*(iv_scaled(n-1,B)+iv_scaled(n+1,B))))
@@ -1491,7 +1491,6 @@ subroutine integrate_ERI_sum(pattern_id,p,q,p_x,q_x,phi,xpA,xpB,xqC,xqD,xa,xb,xc
         term    = exp(I_dp*dble(n)*phi) * termC * termAn * termBn
         if (abs(term) < tol) exit
         sum     = sum + 2.d0 * real(term) * const
-        print*, s2xqcd , B , s2xqcd / B 
       end do
 
 ! case (1112) ! | px  px  px  py   ( 87) 
